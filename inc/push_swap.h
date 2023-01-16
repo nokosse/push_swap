6e3bfaae2824219a0ab50b3a9a8def92d3ff25cb
nokosse <visouthivong.kevin@gmail.com>
2023-01-16 17:21:42 +0100

    init

---
+++ inc/push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 16:41:22 by kvisouth          #+#    #+#             */
+/*   Updated: 2023/01/16 16:45:23 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdint.h>
+# include <fcntl.h>
+# include "../libft/inc/libft.h"
+# include "../libft/inc/ft_printf.h"
+
+#endif
