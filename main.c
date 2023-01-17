0d04ee914a59a8af1069a4896d703b9941b6d169
nokosse <visouthivong.kevin@gmail.com>
2023-01-17 23:27:45 +0100

    fixed main.c

--- main.c
+++ main.c
@@ -6,12 +6,15 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 17:56:32 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/01/17 18:02:11 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/01/17 23:27:12 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/push_swap.h"
+#include "libft/inc/libft.h"
+#include "libft/inc/ft_printf.h"
 
+//Compile like this : "gwww src/* main.c libft/libft.a"
 int main (int ac, char **av)
 {
 	if (ac == 1)
