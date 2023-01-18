cbdb5952c1cd9fc0e22069c998c75714f7d2e2f7
nokosse <visouthivong.kevin@gmail.com>
2023-01-18 17:34:35 +0100

    added max int verification functions

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 16:41:22 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/01/17 17:57:22 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:29:50 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,10 @@ int is_number(char *str);
 int	is_array_numeric(char *str);
 int are_args_numeric(int ac, char **av);
 int	check_args(int ac, char **av);
+int	is_number_bigger_than_max_int(char **av);
+int	is_number_bigger_than_max_int_str(char *str);
 
+// Put args in list
 
 
 #endif
