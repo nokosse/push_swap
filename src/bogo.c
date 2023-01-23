d0a72002d5b13fb3e8a3998c6ee230499262da97
nokosse <visouthivong.kevin@gmail.com>
2023-01-23 14:21:06 +0100

    added sorting files

--- src/bogo.c
+++ src/bogo.c
@@ -1,16 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   algo_3.c                                           :+:      :+:    :+:   */
+/*   pogo_sort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/23 14:09:49 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/01/23 14:20:53 by kvisouth         ###   ########.fr       */
+/*   Created: 2023/01/23 14:18:07 by kvisouth          #+#    #+#             */
+/*   Updated: 2023/01/23 14:18:52 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-// This function will sort the stack_a if it has 3 elements.
-// It must not have more than 3 sorting operations.
+// This function will do random operations until the stack is sorted.
