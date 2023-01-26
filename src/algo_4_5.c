6ecf341f52ee691782214dad14904cce3d595a82
nokosse <visouthivong.kevin@gmail.com>
2023-01-26 13:57:50 +0100

    algo 4 & 5 working

--- src/algo_4_5.c
+++ src/algo_4_5.c
@@ -6,27 +6,73 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 15:13:45 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/01/25 19:36:06 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/01/26 13:52:58 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-// Function algo_5 is called only if stack_a has 5 elements.
-// 1. Do 'pb' 2 times so stack_a has 3 elements.
-// 2. Sort stack_a with algo_3(&a) if its not sorted.
-// To finish the sorting process, we just need to push the last two or less
-// elements on stack B, finding the correct position on the stack_a before
-// running the push operation.
-// The operations have to be used like : pb(&a, &b), sa(&a) etc...
+// How it sorting a list of 4 or 5 elements works ?
+// 1. Find the smallest element in stack_a with find_smallest function.
+// 2. Use ra or rra to put the smallest element on top of stack_a.
+// 3. Push it to stack_b with pb.
+// 4. Repeat this if you have 5 elements.
+// 5. Sort stack_a with algo_3 if not sorted.
+// 6. Just do pa to put the elements back to stack_a
+// 10 operations should be the maximum to sort a list of 5 elements. (2 5 1 4 3)
 
-// typedef struct s_list
-// {
-// 	void			*content;
-// 	struct s_list	*next;
-// }	t_list;
+int	find_smallest(t_list *a)
+{
+	int	smallest;
+
+	smallest = *(int *)a->content;
+	while (a != NULL)
+	{
+		if (*(int *)a->content < smallest)
+			smallest = *(int *)a->content;
+		a = a->next;
+	}
+	return (smallest);
+}
+
+void	algo_4(t_list **a, t_list **b)
+{
+	int	smallest;
+
+	smallest = find_smallest(*a);
+	if (*(int *)(*a)->next->next->next->content == smallest)
+		rra(&*a);
+	while (*(int *)(*a)->content != smallest)
+		ra(&*a);
+	pb(&*a, &*b);
+	if (is_stack_a_sorted_no_b(*a) == 0)
+		algo_3(a);
+	pa(&*a, &*b);
+}
 
 void	algo_5(t_list **a, t_list **b)
 {
-	
+	int	smallest;
+
+	smallest = find_smallest(*a);
+	if (*(int *)(*a)->next->next->next->next->content == smallest)
+		rra(&*a);
+	else if (*(int *)(*a)->next->next->next->content == smallest)
+	{
+		rra(&*a);
+		rra(&*a);
+	}
+	while (*(int *)(*a)->content != smallest)
+		ra(&*a);
+	pb(&*a, &*b);
+	smallest = find_smallest(*a);
+	if (*(int *)(*a)->next->next->next->content == smallest)
+		rra(&*a);
+	while (*(int *)(*a)->content != smallest)
+		ra(&*a);
+	pb(&*a, &*b);
+	if (is_stack_a_sorted_no_b(*a) == 0)
+		algo_3(a);
+	pa(&*a, &*b);
+	pa(&*a, &*b);
 }
--- src/sorting_hub.c
+++ src/sorting_hub.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 15:00:26 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/01/25 19:34:08 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/01/26 13:41:03 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ void	sorting_hub(t_list **stack_a, t_list **stack_b, int params)
 		algo_2(stack_a);
 	else if (params == 3)
 		algo_3(stack_a);
-	// else if (params == 4)
-	// 	algo_4(stack_a, stack_b);
+	else if (params == 4)
+		algo_4(stack_a, stack_b);
 	else if (params == 5)
 		algo_5(stack_a, stack_b);
 }
