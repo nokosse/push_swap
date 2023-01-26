60b90e486b69e2c768520b0388280d203608c8d6
nokosse <visouthivong.kevin@gmail.com>
2023-01-26 20:34:59 +0100

    accepting negatives

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 17:56:32 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/01/26 15:32:25 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/01/26 20:33:46 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,6 @@ void	ft_print_lst(t_list *stack_a)
 	}
 }
 
-int	*nb_init(int ac, char **av)
-{
-	int	*numbers;
-
-	if (ac == 2)
-		numbers = str_to_int_array(av[1]);
-	else if (ac > 2)
-		numbers = av_to_int_array(ac, av);
-	return (numbers);
-}
-
-// Main function of push_swap.
-// 1. Check if arguments are valid.
-// 2. Initialize array of integers.
-// 3. Initialize stack_a with the array of integers.
-// 4. Check if stack_a is sorted.
-// 5. If stack_a is not sorted, call sorting_hub to sort it.
-
 int	main(int ac, char **av)
 {
 	t_list	*stack_a;
@@ -51,12 +33,13 @@ int	main(int ac, char **av)
 		return (0);
 	else if (check_args(ac, av) == 0)
 		return (ft_printf("Error\n"), exit(1), 0);
-	stack_init(nb_init(ac, av), &stack_a);
-	if (is_stack_a_sorted(stack_a, stack_b) == 0)
-		sorting_hub(&stack_a, &stack_b, ac - 1);
-	return (0);
+	printf("OK\n");
+	// stack_init(nb_init(ac, av), &stack_a);
+	// if (is_stack_a_sorted(stack_a, stack_b) == 0)
+	// 	sorting_hub(&stack_a, &stack_b, ac - 1);
+	// return (0);
+	(void)stack_a;
+	(void)stack_b;
 }
 
-//TODO : Edit all the code to use the new structure t_stack
-//TODO : Sort 4 and 5
-//TODO : Edit check_args to accept negative numbers
+//TODO : Edit all the code to use the new structure t_stack instead of t_list
