5afeb9aaf7bdf491e150028f5f082bd4b13fe678
nokosse <visouthivong.kevin@gmail.com>
2023-01-26 15:39:31 +0100

    comment

--- src/sorting_hub.c
+++ src/sorting_hub.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 15:00:26 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/01/26 13:41:03 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/01/26 15:39:24 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,8 @@ void	sorting_hub(t_list **stack_a, t_list **stack_b, int params)
 		algo_4(stack_a, stack_b);
 	else if (params == 5)
 		algo_5(stack_a, stack_b);
+	// else if (params > 5 && params <= 100)
+	// 	algo_100(stack_a, stack_b);
+	// else if (params > 100)
+	// 	algo_500(stack_a, stack_b);
 }
