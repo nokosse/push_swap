38faefef19bc67d4699e7591b0271fbea1f08613
nokosse <visouthivong.kevin@gmail.com>
2023-02-04 12:12:58 +0100

    fixed sending no args error

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 17:56:32 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/02/02 17:05:03 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/02/04 12:07:05 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	main(int ac, char **av)
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
+	if (ac == 1)
+		return (0);
 	stack_a = NULL;
 	stack_b = NULL;
 	if (check_args(ac, av) == 0)
