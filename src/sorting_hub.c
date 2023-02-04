3f008a8d0fb2b1c46be6d96b51fb331618bad917
nokosse <visouthivong.kevin@gmail.com>
2023-02-04 12:33:59 +0100

    added comment

--- src/sorting_hub.c
+++ src/sorting_hub.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 15:00:26 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/02/02 16:33:36 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/02/04 12:32:47 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,11 @@ void	sorting_hub(t_stack **a, t_stack **b, int size)
 		algo_radix(a, b, size);
 	}
 }
+
+// Function algo_2 only use : sa
+// Function algo_3 only use : sa, ra, rra
+// Function algo_4 only use : sa, ra, rra, pb, pa
+// Function algo_5 only use : sa, ra, rra, pb, pa
+// Function radix  only use : ra, pb, pa
+//
+// The remaining instructions in the subject are not used in this project.
